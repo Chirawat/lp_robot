4bc68bb5e9b2d73ee4ab8a1409f2ac0767962dea
chirawat <chirawat.not@gmail.com>
2018-10-11 16:30:44 +0700

    add software servo for XT board

--- line-following-bot/line2/line2.ino
+++ line-following-bot/line2/line2.ino
@@ -2,34 +2,49 @@
  *  Lasted update 2018-10-10
  */
 
-#include <popx2.h>  
+#define XT
+//#define READ_SENSOR
 
-// For  X2
+#ifdef XT
+#include <popxt.h> 
+#include <SoftwareServo.h> 
+SoftwareServo udServo, gripperServo;
+int L_REF=285;
+int C_REF=398;
+int R_REF=426;
+int L2_REF=297;
+int R2_REF=375;
+const int trigPin = 4;
+const int echoPin = 6;
+const int udServoPin = 30;
+const int gripperServoPin = 12;
+int GRIPPER_UD_UP=120;
+int GRIPPER_KEEP=60;
+int GRIPPER_RELEASE=90;
+
+#else
+#include <popx2.h>
 int L_REF=636;
 int R_REF=402;
 int C_REF=620;
 int L2_REF=620;
 int R2_REF=134;
-int KEEP_DISTANCE=5;
-int SPEED=50;
-
-// For XT
-//int L_REF=695;
-//int R_REF=672;
-//int C_REF=642;
-//int L2_REF=642;
-//int R2_REF=642;
-
-int GRIPPER_UD_NORMAL=90;
+const int trigPin = 2;
+const int echoPin = 3;
 int GRIPPER_UD_UP=60;
 int GRIPPER_KEEP=110;
 int GRIPPER_RELEASE=50;
+#endif
 
-const int trigPin = 2;
-const int echoPin = 3;
-long duration_t;
-int L, R, C, L2, R2;
-int error;
+
+
+int     SPEED_t=50;
+int     KEEP_DISTANCE=5;
+int     GRIPPER_UD_NORMAL=90;
+
+long    duration_t;
+int     L, R, C, L2, R2;
+int     error;
 
 void show_sensor_values(){
   glcd(0,0,"L=%d   ", L);
@@ -37,7 +52,6 @@ void show_sensor_values(){
   glcd(2,0,"R=%d   ", R);
   glcd(3,0,"L2=%d   ", L2);
   glcd(4,0,"R2=%d   ", R2);
-
   glcd(5,0,"Error=%d   ", error);
 }
 
@@ -63,20 +77,21 @@ int read_sensor(){
   else{
     error = 0;
   }
-
-  //show_sensor_values();
+  #ifdef READ_SENSOR
+  show_sensor_values();
+  #endif
 }
 
 void follow_line(){
   read_sensor();
   if(error > 0){
-    sl(50);
+    sl(SPEED_t);
   }
   else if(error < 0){
-    sr(50);
+    sr(SPEED_t);
   }
   else{
-    fd(50);
+    fd(SPEED_t);
   }  
 }
 
@@ -92,22 +107,22 @@ void FF(){
 
 void FR(){
   ao(); delay(50);
-  sr(SPEED); delay(500);
+  sr(SPEED_t); delay(300);
   read_sensor();
   while(C > C_REF){
     read_sensor();
-    sr(SPEED);
+    sr(SPEED_t);
   }
   ao();
 }
 
 void FL(){
   ao(); delay(50);
-  sl(SPEED); delay(500);
+  sl(SPEED_t); delay(300);
   read_sensor();
   while(C > C_REF){
     read_sensor();
-    sl(SPEED);
+    sl(SPEED_t);
   }
   ao();
 }
@@ -135,27 +150,33 @@ int read_distance(){
 }
 
 void KEEP(){
-  while(read_distance() > KEEP_DISTANCE){
+  int distance = read_distance();
+  while(distance > KEEP_DISTANCE || distance == 0){
+    distance = read_distance();
     read_sensor();
+    glcd(1,0,"D=%d   ", distance);
     follow_line();
   }
   ao();
   sound(1500, 100);
   delay(1000);
-  servo(2, GRIPPER_KEEP);
+  servo_tt(2, GRIPPER_KEEP);
 }
 
 void LEREASE(){
+  servo_tt(1, GRIPPER_UD_NORMAL);
+  delay(1000);
   servo(2, GRIPPER_RELEASE);
-//  bk(SPEED);
+  delay(1000);
 }
 
 void UP(){
   int pos = GRIPPER_UD_NORMAL;
   delay(1000);
+  #ifdef XT
   for(int i=0; i<300; i++){
-    if(pos > GRIPPER_UD_UP){
-      pos--;
+    if(pos < GRIPPER_UD_UP){
+      pos++;
       servo(1, pos);
     }
     else{
@@ -163,45 +184,81 @@ void UP(){
     }
     delay(30);
   }
+  #else
+  for(int i=0; i<300; i++){
+    if(pos > GRIPPER_UD_UP){
+      pos--;
+      servo_tt(1, pos);
+    }
+    else{
+      break;
+    }
+    delay(30);
+  }
+  #endif
 }
 
 void DOWN(){
-  servo(1, GRIPPER_UD_NORMAL);
+  servo_tt(1, GRIPPER_UD_NORMAL);
+}
+
+void servo_tt(int servo_port, int val){
+  #ifdef XT
+  if(servo_port==1){
+    udServo.write(val);
+  }
+  else if(servo_port==2){
+    gripperServo.write(val);
+  }
+  SoftwareServo::refresh();
+  #else
+  if(servo_port==1){
+    servo(1, val);
+  }
+  else if(servo_port==2){
+    servo(2, val);
+  }
+  #endif
 }
 
 void setup() {
+  #ifdef XT
   glcdMode(3); // For XT
-  glcdMode(1); // For XT
+  udServo.attach(udServoPin);
+  gripperServo.attach(gripperServoPin);
+  
+  // Set gripper to normal position
+  servo_tt(1, GRIPPER_UD_UP);
+  servo_tt(2, GRIPPER_RELEASE);
+  
+  #else
+  glcdMode(1); // For X2
+  // Set gripper to normal position
+  servo_tt(1, GRIPPER_UD_NORMAL);
+  servo_tt(2, GRIPPER_RELEASE);
+  #endif
+  
   setTextSize(2);
   glcd(0,0,"press OK...");
-  
-  glcd(0,0,"started!...");
-
-  // Set gripper to normal position
-  servo(1, GRIPPER_UD_NORMAL);
-  servo(2, GRIPPER_RELEASE);
-
+ 
   // Ultrasocnic
   pinMode (trigPin, OUTPUT);  // Sets the trigPin as an Output
   pinMode (echoPin, INPUT); // Sets the echoPin as an Input
 
-  Serial.begin (9600);
+  //Serial.begin (9600);
   sw_ok_press();
+  glcd(0,0,"started!...");
+  #ifdef READ_SENSOR
+  while(1){
+    read_sensor();
+    glcd(0,8,"D=%d         ", read_distance());
+  }
+  #endif
 }
 
 void loop() {
-
-  KEEP(); UP(); FF(); FL(); FF(); FL();FF();  while(1);
-  
-  
-  
-  
-  //read_distance();
-  
-  //RELEASE(); delay(1000); while(1);
-  //RE(); delay(1000);
-
-  ///////////////////////////////////
-  //glcd(0,0,"Stopped......");
-  //while(1);
+  sl(80);
+  delay(1000);
+  servo_tt(1, 30);
+  while(1);
 }
